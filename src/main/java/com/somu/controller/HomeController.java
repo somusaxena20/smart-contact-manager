355411ad20f3ee6481ec848903debf5f64411732
somusaxena20 <somusaxena20@gmail.com>
2023-08-03 02:19:20 +0530

    jar commit

--- src/main/java/com/somu/controller/HomeController.java
+++ src/main/java/com/somu/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.somu.entity.User;
 
